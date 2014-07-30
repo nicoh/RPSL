1c6f1f65a99cc98b9ab13c3bf413a6d71779bd81
nicoh <nico.hochgeschwender@h-brs.de>
2014-07-30 13:30:48 +0200

    Continued to integrate more model validation (mainly simple constraints)

--- core/rpsl_validation.rb
+++ core/rpsl_validation.rb
@@ -20,11 +20,49 @@ module RpslValidation
         
           assert component.port.each.select{|p| p.class == RpslMetaModel::InputPort}.size >= 1,
             "RpslValidation::ComponentValidation ==> Processing Component " + component.to_s + " does not provide an input port"
-        
         end 
-         
       end
   end
+  
+  class AlgorithmValidation
+    include MiniTest::Assertions
+    
+    def validate_algorithm (algorithm)
+      assert algorithm.name.size != 0,
+        "RpslValidation::AlgorithmValidation ==> Algorithm name of " + algorithm.to_s + " is empty"
+    end
+    
+  end
+  
+  class PerceptionGraphValidation
+    include MiniTest::Assertions
+    
+    def validate_perception_graph (perception_graph)
+      assert perception_graph.name.size != 0,
+        "RpslValidation::PerceptionGraphValidation ==> Perception Graph name of " + perception_graph.to_s + " is empty"
+    end
+  end
+  
+  class ConceptValidation
+    include MiniTest::Assertions
+    
+    def validate_concept (concept)
+      assert concept.name.size != 0,
+        "RpslValidation::ConceptValidation ==> Concept name of " + concept.to_s + " is empty"
+    end
+    
+  end
+
+  class PrototypeValidation
+    include MiniTest::Assertions
+    
+    def validate_prototype (prototype)
+      assert prototype.name.size != 0,
+      "RpslValidation::PrototypeValidation ==> Prototype name of " + prototype.to_s + " is empty"
+    end
+    
+  end
+    
 end
 
 i = RpslMetaModel::InputPort.new()
